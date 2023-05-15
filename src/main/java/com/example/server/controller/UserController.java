1ad2eae24b31e09c63947f4d5669f9bcf88f18b3
OngDuyThang <duythangong@gmail.com>
2023-05-15 16:51:04 +0700

    first commit

---
+++ src/main/java/com/example/server/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.server.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.server.model.User;
+
+import jakarta.validation.Valid;
+
+//@CrossOrigin(origins = "https://ongduythangclient.azurewebsites.net")
+@CrossOrigin
+@RestController
+public class UserController {
+
+	@PostMapping("/login")
+	public String login(@Valid @RequestBody User user) {
+		return user.getUserName().equals("admin") && user.getPassword().equals("admin")
+		? "success"
+		: "fail";
+	}
+	
+}
